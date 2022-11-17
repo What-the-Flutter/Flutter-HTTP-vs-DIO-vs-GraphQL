e2e73c1b429eb250b6c0f402f43c426cea28f857
topaffiliate <topaffiliate1304@gmail.com>
2022-11-17 22:10:34 +0100

    rollback connectivity_constants

--- client/lib/domain/constants/connectivity_constants.dart
+++ client/lib/domain/constants/connectivity_constants.dart
@@ -3,7 +3,7 @@ abstract class ConnectivityStrings {
     'NETWORK_MODULE',
     defaultValue: 'http',
   );
-  static const _baseDomain = '10.10.28.55';
+  static const _baseDomain = 'localhost';
   static final int _port = networkModule == NetworkModule.graphql.name ? 5000 : 5001;
   static final String _domain = networkModule == NetworkModule.graphql.name ? 'graphql' : 'api';
   static final String baseUrl = 'http://$_baseDomain:$_port/$_domain';
